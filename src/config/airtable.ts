8824d906ca85fb024d5c5725aaf0c28c27ef8c6e
derfaule <51172680+derfaule@users.noreply.github.com>
2025-08-23 23:49:46 -0500

    Updated airtable.ts

--- src/config/airtable.ts
+++ src/config/airtable.ts
@@ -1,7 +1,7 @@
 // Airtable Configuration
 export const AIRTABLE_CONFIG = {
   BASE_ID: 'appVYEMXc3C7Qc32u', // Your Airtable Base ID
-  accessToken: 'patVKKpTTPwGejb4S', // Replace with your Personal Access Token (PAT)
+  accessToken: 'pat1FoVPSncGVJWb2.1527add7d762e8fcd364daeaa5a6442bd7c75242fb1c27568f61d7f1a68293f3', // Replace with your Personal Access Token (PAT)
   TABLE_NAME: 'Imported_table', // Replace with your actual table name
   API_URL: `https://api.airtable.com/v0`
 };
